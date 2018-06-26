5badda252f8e1e0a169b413bd74709b5aad41e2b
Natalie <nolivo1@gmail.com>
2018-06-26 07:24:53 -0400

    initial web component

---
+++ page-head.js
@@ -0,0 +1,15 @@
+// component functionality is encapsulated here
+class PageHead extends HTMLElement {
+  constructor() {
+    // Always call super first in constructor
+    super();
+
+    // Element functionality written in here
+    let template = document.getElementById('page-head');
+    let templateContent = template.content;
+    const shadowRoot = this.attachShadow({mode: 'open'}).appendChild(templateContent.cloneNode(true));
+  }
+}
+
+// Define the new element
+customElements.define('page-head', PageHead);
