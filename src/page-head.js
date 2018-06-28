b982bef0cc379fcf219adba890ba25dc16fe3428
Zakir Tariverdiev <ztariverdiev@businessinsider.com>
2018-06-28 12:54:59 -0400

    Use external HTML template for page-ghead component

--- src/page-head.js
+++ src/page-head.js
@@ -1,14 +1,25 @@
 // component functionality is encapsulated here
 class PageHead extends HTMLElement {
-  constructor() {
-    // Always call super first in constructor
-    super();
+    constructor() {
+        // Always call super first in constructor
+        super();
 
-    // Element functionality written in here
-    let template = document.getElementById('page-head');
-    let templateContent = template.content;
-    const shadowRoot = this.attachShadow({ mode: 'open' }).appendChild(templateContent.cloneNode(true));
-  }
+        this._getPageHeadTemplate().then((html) => {
+            // Element functionality written in here
+            const domParser = new DOMParser();
+            const templateDom = domParser.parseFromString(html, 'text/html');
+            const template = templateDom.getElementById('page-head');
+            if (template) {
+                const templateContent = template.content;
+                const shadowRoot = this.attachShadow({ mode: 'open' }).appendChild(templateContent.cloneNode(true));
+            }
+        });
+    }
+
+    async _getPageHeadTemplate() {
+        const html = await (await fetch('page-head.component.html')).text();
+        return html;
+    }
 }
 
 // Define the new element
